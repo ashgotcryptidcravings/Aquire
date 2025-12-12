d44fe97f3e028e072c61301b55b71669a1cac1c5
ashgotcryptidcravings <217374789+ashgotcryptidcravings@users.noreply.github.com>
2025-12-11 23:10:08 -0500

    Lots of little improvements, no major changes.
    -Optimization Centered
    -Reworked file structure
    -Couple QOL improvements
    
    ~Directing focus toward major update~

--- Aquire/NarrativeModels.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NarrativeModels.swift
-//  Aquire
-//
-//  Created by Zero on 12/10/25.
-//
-
-import Foundation
---
+++ Aquire/Utilities/SearchBarView.swift
@@ -0,0 +1,8 @@
+//
+//  Searchbarview.swift
+//  Aquire
+//
+//  Created by Zero on 12/11/25.
+//
+
+import Foundation
